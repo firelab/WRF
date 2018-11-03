58184397a4279047147f1a06d0ede4553b598e51
root <root@evilwxdude.usfs-i2.umt.edu>
2018-11-03 17:05:47 -0600

    completed conversion to gnu7 and left gnu commented out

--- 000_bryce_build.sh
+++ 000_bryce_build.sh
@@ -1,5 +1,6 @@
 module load gnu7
 module load openmpi3
+#module load gnu
 #module load openmpi
 module load netcdf netcdf-fortran
 
@@ -7,6 +8,7 @@ export NETCDF=/opt/ohpc/pub/libs/gnu7/openmpi3/netcdf/4.6.1
 export NETCDF=/opt/ohpc/pub/libs/gnu7/openmpi3/netcdf-fortran/4.4.4
 export NETCDF_classic=1
 export CPP=/opt/ohpc/pub/compiler/gcc/7.3.0/bin/cpp
+export LIB_LOCAL=/opt/ohpc/pub/libs/gnu7/openmpi3/netcdf/4.6.1/bin/ncdump
 #export CPP=/opt/ohpc/pub/compiler/gcc/5.4.0/bin/cpp
 #export NETCDF=/opt/ohpc/pub/libs/gnu/openmpi/netcdf-fortran/4.4.4
-export LIB_LOCAL=/opt/ohpc/pub/libs/gnu/openmpi/netcdf/4.5.0/lib
+#export LIB_LOCAL=/opt/ohpc/pub/libs/gnu/openmpi/netcdf/4.5.0/lib
